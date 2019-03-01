38a3ad3f2c82462de9cb9d5529a7a4ce312f9ce0
Naomi <nchristie5@bloomberg.net>
2019-03-01 18:05:04 +0000

    code which runs in repl.it and on local machine

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-"""
+'''
 Press run above to start
-"""
+'''
 
-if input("\n\nPress enter to start\n") != "test":
-    from exercises import run
-
-    run()
-# else:
-#     from test_questions import *
-
-#     if __name__ == "__main__":
-#         unittest.main()
+if input('\n\nPress enter to start\n') != 'test':
+  from exercises.question_runner import run
+  run()
+else:
+  from unit_tests.test_instructor_code import *
+  if __name__ == '__main__':
+    unittest.main()
